fb1658ac7ba6a2b78b47126dc29b2bcf29b22edb
Alison Jenkins <alison.juliet.jenkins@gmail.com>
2023-09-27 06:23:02 +0100

    Get rid of hyprland command that breaks sddm

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -18,16 +18,6 @@
     };
   };
 
-  programs = {
-    bash = {
-      interactiveShellInit = ''
-        if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
-           WLR_NO_HARDWARE_CURSORS=1 Hyprland #prevents cursor disappear when using Nvidia drivers
-        fi
-      '';
-    };
-  };
-
   programs.dconf.enable = true;
 
   xdg.portal = {
