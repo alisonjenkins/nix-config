9516eae90f672f28f33a77a1f0d5f3de0219be97
Alison Jenkins <alison.juliet.jenkins@gmail.com>
2023-09-27 06:28:46 +0100

    Get rid of greetd for now

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -7,17 +7,6 @@
     ./virtualisation
   ];
 
-  programs.regreet.enable = true;
-  services.greetd = {
-    enable = true;
-    settings = {
-      initial_session = {
-        user = "ali";
-        command = "$SHELL -l";
-      };
-    };
-  };
-
   programs.dconf.enable = true;
 
   xdg.portal = {
