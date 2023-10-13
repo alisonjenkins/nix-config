50d228471ebd477ae5749835cca42768d29aebc4
Alison Jenkins <alison.juliet.jenkins@gmail.com>
2023-10-13 18:09:25 +0100

    Make tuigreet remember user and session

--- app-profiles/desktop/display-managers/greetd/default.nix
+++ app-profiles/desktop/display-managers/greetd/default.nix
@@ -5,7 +5,7 @@
       enable = true;
       settings = {
         default_session = {
-          command = "${pkgs.greetd.tuigreet}/bin/tuigreet";
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --remember-user-session";
         };
       };
     };
