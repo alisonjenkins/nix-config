a7980148dee6dfd8eaf825435506ea9faf209716
Alison Jenkins <alison.juliet.jenkins@gmail.com>
2023-10-21 10:02:24 +0100

    Add time to tuigreet

--- app-profiles/desktop/display-managers/greetd/default.nix
+++ app-profiles/desktop/display-managers/greetd/default.nix
@@ -1,15 +1,19 @@
-{ pkgs, ... }:
+{ config, pkgs, lib, system, ... }:
 {
   services = {
     greetd = {
       enable = true;
       settings = {
         default_session = {
-          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --remember-user-session --sessions /run/booted-system/sw/share/xsessions/:/run/booted-system/sw/share/wayland-sessions/";
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-user-session --sessions /run/booted-system/sw/share/xsessions/:/run/booted-system/sw/share/wayland-sessions/";
         };
       };
     };
   };
+
+  system.activationScripts.makeTuigreetCacheDir = lib.stringAfter [ "var" ] ''
+    mkdir -p /var/cache/tuigreet
+  '';
 
   environment.systemPackages = with pkgs; [
     greetd.tuigreet
