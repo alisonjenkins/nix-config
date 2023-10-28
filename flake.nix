96ce34b5402e60021fe68f00dc703a6b26272753
Alison Jenkins <alison.juliet.jenkins@gmail.com>
2023-10-28 15:27:02 +0100

    Add dev-vm

--- flake.nix
+++ flake.nix
@@ -57,6 +57,24 @@
             }
           ];
         };
+
+        dev-vm = lib.nixosSystem rec {
+          inherit system;
+          specialArgs = { inherit hyprland; };
+          modules = [
+            ./hosts/dev-vm/configuration.nix
+            #./app-profiles/desktop/display-managers/greetd
+            #./app-profiles/desktop/wms/plasma5
+            #hyprland.nixosModules.default
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.ali = import ./home/home.nix;
+              home-manager.extraSpecialArgs = specialArgs;
+            }
+          ];
+        };
       };
     };
 }
