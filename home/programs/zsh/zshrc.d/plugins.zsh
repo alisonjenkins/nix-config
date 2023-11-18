68686bb750dd7085bf698688a5f58648eb68647a
Alison Jenkins <alison.juliet.jenkins@gmail.com>
2023-11-18 13:04:18 +0000

    Cleanup plugins.zsh

--- home/programs/zsh/zshrc.d/plugins.zsh
+++ home/programs/zsh/zshrc.d/plugins.zsh
@@ -31,64 +31,20 @@ zinit load macunha1/zsh-terraform
 zinit load zsh-users/zsh-autosuggestions
 zinit load molovo/tipz
 
-# Install rtx version manager (replacement for asdf)
-# zinit ice from"gh-r" as"command" mv"rtx* -> rtx" \
-#   atclone'./rtx complete -s zsh > _rtx' atpull'%atclone'
-# zinit light jdxcode/rtx
-# eval "$(rtx activate zsh)"
-
 # Setup Vi mode
 zinit ice depth=1
 zinit light jeffreytse/zsh-vi-mode
 
-# install zoxide
-# rtx global zoxide@0.9.0 &>/dev/null
-zinit light ajeetdsouza/zoxide
-
-# Setup direnv
-# eval "$(rtx exec direnv -- direnv hook zsh)"
-
-# A shortcut for asdf managed direnv.
-# direnv() { rtx exec direnv -- direnv "$@"; }
-eval "$(direnv hook zsh)"
-
 zinit snippet 'https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/aws/aws.plugin.zsh'
 zinit snippet 'https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/command-not-found/command-not-found.plugin.zsh'
 
-# For postponing loading `fzf`
+eval "$(zoxide init zsh)"
+eval "$(direnv hook zsh)"
+eval "$(mcfly init zsh)"
+
 zinit ice lucid wait
 zinit snippet OMZP::fzf
 
-# # Fix Mcfly downloading the wrong binary
-# case $(uname) in
-#   Darwin)
-#     case $(uname -m) in
-#       x86_64)
-#         mcfly_os="*x86_64-apple-darwin*"
-#       ;;
-#       arm64)
-#         mcfly_os="*aarch64*darwin*"
-#       ;;
-#     esac
-#   ;;
-#   Linux)
-#     case $(uname -m) in
-#       x86_64)
-#         mcfly_os="*x86_64*linux*musl*"
-#       ;;
-#       arm64)
-#         mcfly_os="*aarch64*linux*"
-#       ;;
-#     esac
-#   ;;
-# esac
-#
-# if [[ "$mcfly_os" == "*aarch64*darwin*" ]]; then
-#   zinit ice lucid wait"0a" as"program" atclone"cargo build --release" pick"./target/release/mcfly" atload'eval "$(mcfly init zsh)"'
-# else
-#   zinit ice lucid wait"0a" from"gh-r" as"program" atload'eval "$(mcfly init zsh)"' bpick"${mcfly_os}"
-# fi
-zinit light cantino/mcfly
 
 autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
