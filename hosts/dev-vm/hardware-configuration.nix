e4ea93940dcdde6d3fcc1702d953a014216bc933
Alison Jenkins <alison.juliet.jenkins@gmail.com>
2023-12-05 11:49:05 +0000

    Add back filesystems for dev-vm

--- hosts/dev-vm/hardware-configuration.nix
+++ hosts/dev-vm/hardware-configuration.nix
@@ -21,6 +21,17 @@
     };
   };
 
+  fileSystems = {
+    "/" = {
+      device = "/dev/pool/root";
+      fsType = "ext4";
+    };
+    "/boot" = {
+      device = "/dev/vda1";
+      fsType = "vfat";
+    };
+  };
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
