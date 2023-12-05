944ac976c5cd6e994089f2acda52c2518dbd5cf0
Alison Jenkins <alison.juliet.jenkins@gmail.com>
2023-12-05 11:37:23 +0000

    Fix extra brackets in disko config for dev-vm

--- hosts/dev-vm/disko-config.nix
+++ hosts/dev-vm/disko-config.nix
@@ -1,34 +1,32 @@
 {
-  {
-    disko.devices = {
-      disk = {
-        vda = {
-          device = "/dev/vda";
-          type = "disk";
-          content = {
-            type = "gpt";
-            partitions = {
-              ESP = {
-                type = "EF00";
-                size = "500M";
-                content = {
-                  type = "filesystem";
-                  format = "vfat";
-                  mountpoint = "/boot";
-                };
+  disko.devices = {
+    disk = {
+      vda = {
+        device = "/dev/vda";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "500M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
               };
-              root = {
-                size = "100%";
-                content = {
-                  type = "filesystem";
-                  format = "ext4";
-                  mountpoint = "/";
-                };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
               };
             };
           };
         };
       };
     };
-  }
+  };
 }
