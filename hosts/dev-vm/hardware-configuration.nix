be095c497c75e0a797686215f2f2f2bf619e2527
Alison Jenkins <alison.juliet.jenkins@gmail.com>
2023-12-05 10:54:29 +0000

    Fix dev-vm hostPlatform

--- hosts/dev-vm/hardware-configuration.nix
+++ hosts/dev-vm/hardware-configuration.nix
@@ -28,6 +28,6 @@
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp16s0.useDHCP = lib.mkDefault true;
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
