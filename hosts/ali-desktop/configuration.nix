06bf0cc0f39919ca12677d49bca588edf4a3476f
Alison Jenkins <alison.juliet.jenkins@gmail.com>
2023-12-16 14:47:26 +0000

    Remove efiInstallAsRemovable

--- hosts/ali-desktop/configuration.nix
+++ hosts/ali-desktop/configuration.nix
@@ -13,7 +13,6 @@
       grub = {
         enable = true;
         devices = [ "nodev" ];
-        efiInstallAsRemovable = true;
         efiSupport = true;
         useOSProber = true;
         theme = pkgs.stdenv.mkDerivation {
