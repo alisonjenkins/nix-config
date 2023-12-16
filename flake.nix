b975e54b0e34a6b6d94b7c6e53f807fafd119ff5
Alison Jenkins <alison.juliet.jenkins@gmail.com>
2023-12-16 20:58:43 +0000

    Setup chaotic

--- flake.nix
+++ flake.nix
@@ -31,6 +31,7 @@
             ./app-profiles/desktop/display-managers/greetd
             ./app-profiles/desktop/wms/plasma5
             ./app-profiles/desktop/wms/hypr
+            chaotic.nixosModules.default
             hyprland.nixosModules.default
             home-manager.nixosModules.home-manager
             {
