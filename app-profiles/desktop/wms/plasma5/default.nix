ed3e04725400d2547809cfdaa371fcaa75c2c433
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2023-12-21 12:57:45 +0000

    Add plasma5 browser integration

--- app-profiles/desktop/wms/plasma5/default.nix
+++ app-profiles/desktop/wms/plasma5/default.nix
@@ -5,6 +5,7 @@
 
   programs.dconf.enable = true;
   programs.kdeconnect.enable = true;
+  environment.systemPackages = with pkgs; [ plasma-browser-integration ];
 
   networking.firewall = {
     enable = true;
