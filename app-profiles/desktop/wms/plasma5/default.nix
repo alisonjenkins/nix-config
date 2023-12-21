e131a4cbea40b8d252fdbec1a922dfb99b7aa62f
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2023-12-21 12:58:00 +0000

    Format plasma5 default.nix

--- app-profiles/desktop/wms/plasma5/default.nix
+++ app-profiles/desktop/wms/plasma5/default.nix
@@ -10,10 +10,10 @@
   networking.firewall = {
     enable = true;
     allowedTCPPortRanges = [
-     { from = 1714; to = 1764; } # KDE Connect
+      { from = 1714; to = 1764; } # KDE Connect
     ];
     allowedUDPPortRanges = [
-     { from = 1714; to = 1764; } # KDE Connect
+      { from = 1714; to = 1764; } # KDE Connect
     ];
   };
 
