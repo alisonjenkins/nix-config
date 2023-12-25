600097bf65908b12046f5f0748aefba99e02335f
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2023-12-25 11:30:51 +0000

    Tidy up hardware-configuration for home-kvm-hypervisor-1

--- hosts/home-kvm-hypervisor-1/hardware-configuration.nix
+++ hosts/home-kvm-hypervisor-1/hardware-configuration.nix
@@ -27,36 +27,11 @@
     initrd = {
       availableKernelModules = [ "xhci_pci" "nvme" "ahci" "uas" "usbhid" "usb_storage" "sd_mod" "sr_mod" "virtio_blk" "ehci_pci" "cryptd" "virtio_pci" ];
       kernelModules = [ "dm-snapshot" ];
-    #   luks.devices.luksroot =
-    #     {
-    #       device = "/dev/disk/by-partlabel/vg";
-    #       preLVM = true;
-    #       allowDiscards = true;
-    #     };
     };
   };
 
-  # fileSystems = {
-  #   "/" = {
-  #     device = "/dev/os_raid1/root";
-  #     fsType = "ext4";
-  #   };
-  #   "/boot" = {
-  #     device = "/dev/disk/by-partlabel/esp";
-  #     fsType = "vfat";
-  #   };
-  # };
-  #
-  # swapDevices = [
-  #   { device = "/dev/os_raid1/swap"; }
-  # ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp16s0.useDHCP = lib.mkDefault true;
+  networking.interfaces.enp16s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
