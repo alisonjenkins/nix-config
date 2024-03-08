78d4caeaacb80e3f5a8f3ec3d1323abb32340fab
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-03-08 22:20:03 +0000

    Tweaks

--- hosts/ali-desktop/hardware-configuration.nix
+++ hosts/ali-desktop/hardware-configuration.nix
@@ -110,6 +110,15 @@
 
   swapDevices = [{ device = "/dev/osvg/swap"; }];
 
+  hardware = {
+    cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+    };
+  };
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
@@ -117,12 +126,8 @@
   networking = {
     hostId = "77f0d408";
     useDHCP = lib.mkDefault true;
+    interfaces.enp16s0.useDHCP = lib.mkDefault true;
   };
-  # networking.interfaces.enp16s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode =
-    lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
-
-
