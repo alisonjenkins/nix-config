b888ca83c7786227b0af8d041e43835d68092bf9
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-03-17 10:54:50 +0000

    Remove Hyprland envvars

--- home/home.nix
+++ home/home.nix
@@ -53,17 +53,6 @@
     eog
   ]);
 
-  home.sessionVariables = {
-    GDK_BACKEND = "wayland,x11";
-    QT_QPA_PLATFORM = "wayland;xcb";
-    #SDL_VIDEODRIVER = "x11";
-    CLUTTER_BACKEND = "wayland";
-    XDG_CURRENT_DESKTOP = "Hyprland";
-    XDG_SESSION_TYPE = "wayland";
-    XDG_SESSION_DESKTOP = "Hyprland";
-    WLR_NO_HARDWARE_CURSORS = "1";
-  };
-
   programs.home-manager.enable = true;
   targets.genericLinux.enable = true;
 
