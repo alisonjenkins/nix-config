4d775689503424bb3bafdfcd1619146480ec4204
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-03-26 12:05:28 +0000

    Fix mcfly

--- home/programs/mcfly/default.nix
+++ home/programs/mcfly/default.nix
@@ -3,5 +3,8 @@
     enable = true;
     fuzzySearchFactor = 3;
     keyScheme = "vim";
+    enableBashIntegration = true;
+    enableFishIntegration = true;
+    enableZshIntegration = true;
   };
 }
