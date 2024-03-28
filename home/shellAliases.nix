51d6326de9c727813a9c4e525e2147848d60a932
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-03-28 08:43:41 +0000

    Add more aliases

--- home/shellAliases.nix
+++ home/shellAliases.nix
@@ -1,3 +1,27 @@
 {
-  j = "just";
+  "-- -" = "cd -";
+  ".." = "cd ..";
+  "..." = "cd ../..";
+  "...." = "cd ../../..";
+  "....." = "cd ../../../..";
+  "cdd" = "cd ~/Downloads/";
+  "cdg" = "cd ~/git/";
+  "cdgo" = "cd \$GOPATH";
+  "cdot" = "cd ~/.local/share/chezmoi";
+  "j" = "just";
+  "key" = "ssh-add ~/.ssh/ssh_keys/id_bashton_alan";
+  "keyaur" = "ssh-add ~/.ssh/ssh_keys/id_aur";
+  "keyb" = "ssh-add ~/.ssh/ssh_keys/id_bashton";
+  "keycl" = "ssh-add -D";
+  "keyk" = "ssh-add ~/.ssh/ssh_keys/id_krystal";
+  "keyp" = "ssh-add ~/.ssh/ssh_keys/id_personal";
+  "keypa" = "ssh-add ~/.ssh/ssh_keys/id_alan-aws";
+  "keypo" = "ssh-add ~/.ssh/ssh_keys/id_personal_old";
+  "kmse" = "export EYAML_CONFIG=$PWD/.kms-eyaml.yaml";
+  "ll" = "eza -l --grid --git";
+  "ls" = "eza";
+  "lt" = "eza --tree --git --long";
+  "pwhash" = "python -c \"import crypt,getpass; print(crypt.crypt(getpass.getpass(), crypt.mksalt(crypt.METHOD_SHA512)))\"";
+  "vi" = "nvim";
+  "vim" = "nvim";
 }
