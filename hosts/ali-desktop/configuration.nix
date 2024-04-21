a105d8603c866a828c73e01dfc9898ca5276d7de
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-04-21 17:04:21 +0100

    Swap to stable linux kernel on ali-desktop

--- hosts/ali-desktop/configuration.nix
+++ hosts/ali-desktop/configuration.nix
@@ -21,7 +21,9 @@
   boot = {
     consoleLogLevel = 0;
     initrd.verbose = false;
-    kernelPackages = pkgs.linuxPackages_cachyos;
+    kernelPackages = pkgs.linuxPackages;
+    # kernelPackages = pkgs.linuxPackages_latest;
+    # kernelPackages = pkgs.linuxPackages_cachyos;
     # kernelPackages = pkgs.linuxPackages_xanmod;
     kernelParams = [
       "quiet"
