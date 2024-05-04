85d2593073f11d43fb171e1ff9ddc1779245993c
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-05-04 12:16:55 +0100

    Swap to realtime kernel for ali-desktop

--- hosts/ali-desktop/configuration.nix
+++ hosts/ali-desktop/configuration.nix
@@ -21,7 +21,8 @@
   boot = {
     consoleLogLevel = 0;
     initrd.verbose = false;
-    kernelPackages = pkgs.linuxPackages;
+    kernelPackages = pkgs.linuxPackages-rt_latest;
+    # kernelPackages = pkgs.linuxPackages;
     # kernelPackages = pkgs.linuxPackages_cachyos;
     # kernelPackages = pkgs.linuxPackages_latest;
     # kernelPackages = pkgs.linuxPackages_xanmod;
