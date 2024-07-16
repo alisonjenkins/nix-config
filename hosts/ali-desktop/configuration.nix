e305938026489f80d6908d98795a8ad4c3da8303
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-07-16 23:28:12 +0100

    Format ali-desktop configuration with alejandra

--- hosts/ali-desktop/configuration.nix
+++ hosts/ali-desktop/configuration.nix
@@ -282,18 +282,18 @@
       package = pkgs.steam.override {
         extraEnv = {};
         extraLibraries = pkgs:
-        with pkgs; [
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXinerama
-          xorg.libXScrnSaver
-          libpng
-          libpulseaudio
-          libvorbis
-          stdenv.cc.cc.lib
-          libkrb5
-          keyutils
-        ];
+          with pkgs; [
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXinerama
+            xorg.libXScrnSaver
+            libpng
+            libpulseaudio
+            libvorbis
+            stdenv.cc.cc.lib
+            libkrb5
+            keyutils
+          ];
       };
       gamescopeSession = {
         enable = true;
