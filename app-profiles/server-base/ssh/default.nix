9b0584165519c73b9c03be8e22c1ffec39992d03
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-07-20 16:49:12 +0100

    Setup SSH hostKeys in server profile

--- app-profiles/server-base/ssh/default.nix
+++ app-profiles/server-base/ssh/default.nix
@@ -1,13 +1,21 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}: {
+{...}: {
   services.openssh = {
     enable = true;
     settings.PasswordAuthentication = false;
     settings.KbdInteractiveAuthentication = false;
     settings.PermitRootLogin = "no";
+    hostKeys = [
+      {
+        bits = 4096;
+        path = "/etc/ssh/ssh_host_rsa_key";
+        rounds = 100;
+        type = "rsa";
+      }
+      {
+        path = "/etc/ssh/ssh_host_ed25519_key";
+        rounds = 100;
+        type = "ed25519";
+      }
+    ];
   };
 }
