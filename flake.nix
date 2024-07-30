d1d365a72ac0c84e73558bb7229c8f7bdf382b0d
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-07-30 17:23:19 +0100

    Swap ali-desktop to using greetd from sddm
    SDDM tends to break causing issues.

--- flake.nix
+++ flake.nix
@@ -92,9 +92,9 @@
           inherit system;
         };
         modules = [
-          ./app-profiles/desktop/display-managers/sddm
+          # ./app-profiles/desktop/display-managers/sddm
           ./app-profiles/desktop/aws
-          # ./app-profiles/desktop/display-managers/greetd
+          ./app-profiles/desktop/display-managers/greetd
           ./app-profiles/desktop/wms/plasma6
           ./app-profiles/desktop/wms/sway
           ./app-profiles/hardware/vr
