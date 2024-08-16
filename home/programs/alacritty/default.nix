417da586bf3b97c7e8d1bbd971ef54346a5e558f
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-08-16 22:58:56 +0100

    Ensure alacritty has a .desktop file

--- home/programs/alacritty/default.nix
+++ home/programs/alacritty/default.nix
@@ -2,9 +2,59 @@
   config,
   pkgs,
   lib,
+  username,
+  inputs,
+  system,
   ...
 }: {
   home.packages = lib.optionals config.programs.alacritty.enable [(pkgs.nerdfonts.override {fonts = ["FiraCode" "Hack" "JetBrainsMono"];})];
+
+  home.file =
+    if config.programs.alacritty.enable && pkgs.stdenv.isLinux && username == "deck"
+    then {
+      ".local/share/applications/Alacritty.desktop".text = ''
+        [Desktop Entry]
+        Comment[en_US]=
+        Comment=
+        Exec=${inputs.nixgl.packages.${system}.nixGLIntel}/bin/nixGLIntel ${pkgs.alacritty}/bin/alacritty
+        GenericName[en_US]=
+        GenericName=
+        Icon=${pkgs.alacritty}/share/icons/hicolor/scalable/apps/Alacritty.svg
+        MimeType=
+        Name[en_US]=Alacritty
+        Name=Alacritty
+        Path=
+        StartupNotify=true
+        Terminal=false
+        TerminalOptions=
+        Type=Application
+        X-KDE-SubstituteUID=false
+        X-KDE-Username=
+      '';
+    }
+    else if config.programs.alacritty.enable && pkgs.stdenv.isLinux
+    then {
+      ".local/share/applications/Alacritty.desktop".text = ''
+        [Desktop Entry]
+        Comment[en_US]=
+        Comment=
+        Exec=${pkgs.alacritty}/bin/alacritty
+        GenericName[en_US]=
+        GenericName=
+        Icon=${pkgs.alacritty}/share/icons/hicolor/scalable/apps/Alacritty.svg
+        MimeType=
+        Name[en_US]=Alacritty
+        Name=Alacritty
+        Path=
+        StartupNotify=true
+        Terminal=false
+        TerminalOptions=
+        Type=Application
+        X-KDE-SubstituteUID=false
+        X-KDE-Username=
+      '';
+    }
+    else {};
 
   programs.alacritty = {
     enable = true;
