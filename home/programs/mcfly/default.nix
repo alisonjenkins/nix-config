758ee6b358945eaea43000c73429a7bf54dc123a
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-08-17 08:35:41 +0100

    Fix mcfly

--- home/programs/mcfly/default.nix
+++ home/programs/mcfly/default.nix
@@ -1,7 +1,25 @@
-{...}: {
-  programs.mcfly = {
-    enable = true;
-    fuzzySearchFactor = 3;
-    keyScheme = "vim";
+{pkgs, ...}: let
+  mcfly_init = pkgs.stdenv.mkDerivation {
+    name = "mcfly-init";
+
+    dontUnpack = true;
+    buildInputs = [pkgs.unstable.mcfly];
+
+    buildPhase = ''
+      mcfly init zsh > $out
+    '';
   };
+  zshViModeEnabled = true;
+in {
+  programs.zsh.initExtra =
+    if zshViModeEnabled
+    then ''
+      function mcfly_init() {
+        source ${mcfly_init}
+      }
+      zvm_after_init_commands+=(mcfly_init)
+    ''
+    else ''
+      source ${mcfly_init}
+    '';
 }
