8535985de0ebb7415ef5b93099e3150b478ee95f
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-09-08 20:28:42 +0100

    1password desktop file

--- home/programs/1password/default.nix
+++ home/programs/1password/default.nix
@@ -7,4 +7,29 @@
         _1password-gui
       ]
     else [];
+
+  home.file =
+    if pkgs.stdenv.isLinux
+    then {
+      ".local/share/applications/1Password.desktop".text = ''
+        [Desktop Entry]
+        Comment[en_US]=
+        Comment=
+        Exec=${pkgs._1password-gui}/bin/1password
+        GenericName[en_US]=
+        GenericName=
+        Icon=${pkgs._1password-gui}/share/icons/hicolor/512x512/apps/1password.png
+        MimeType=
+        Name[en_US]=1Password
+        Name=1Password
+        Path=
+        StartupNotify=true
+        Terminal=false
+        TerminalOptions=
+        Type=Application
+        X-KDE-SubstituteUID=false
+        X-KDE-Username=
+      '';
+    }
+    else {};
 }
