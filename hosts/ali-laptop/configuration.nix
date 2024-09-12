0941799c0a4923c68d017486e9fb285d1c37b1ce
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-09-12 06:52:16 +0100

    Add specialisations to ali-laptop to turn of the hot nvidia gpu

--- hosts/ali-laptop/configuration.nix
+++ hosts/ali-laptop/configuration.nix
@@ -127,7 +127,6 @@
         "fbdev"
         "intel"
         "modesetting"
-        "nvidia"
       ];
       xkb = {
         layout = "us";
@@ -137,7 +136,33 @@
   };
 
   specialisation = {
+    nvidia-graphics-completely-disabled.configuration = {
+      boot.extraModprobeConfig = ''
+        blacklist nouveau
+        options nouveau modeset=0
+      '';
+
+      services.udev.extraRules = ''
+        # Remove NVIDIA USB xHCI Host Controller devices, if present
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+        # Remove NVIDIA USB Type-C UCSI devices, if present
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+        # Remove NVIDIA Audio devices, if present
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+        # Remove NVIDIA VGA/3D controller devices
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+      '';
+      boot.blacklistedKernelModules = ["nouveau" "nvidia" "nvidia_drm" "nvidia_modeset"];
+    };
+
     nvidia-graphics-enabled.configuration = {
+      services.xserver.videoDrivers = [
+        "fbdev"
+        "intel"
+        "modesetting"
+        "nvidia"
+      ];
+
       hardware.nvidia = {
         modesetting.enable = true;
         nvidiaSettings = true;
