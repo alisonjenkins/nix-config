a283d320a528972b48727b12f458a879c9a4b45c
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-09-16 08:36:09 +0100

    Disable obs advanced-scene switcher plugin
    Current doesn't compile

--- home/programs/obs/default.nix
+++ home/programs/obs/default.nix
@@ -8,7 +8,7 @@
   programs.obs-studio = {
     enable = lib.mkIf pkgs.stdenv.isLinux true;
     plugins = with pkgs.obs-studio-plugins; [
-      inputs.nixpkgs.legacyPackages.${system}.obs-studio-plugins.advanced-scene-switcher
+      # inputs.nixpkgs.legacyPackages.${system}.obs-studio-plugins.advanced-scene-switcher
       droidcam-obs
       inputs.nixpkgs.legacyPackages.${system}.obs-studio-plugins.obs-backgroundremoval
       obs-pipewire-audio-capture
