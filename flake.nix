ab378bba5e31f17953df4f60118dc479ef046f56
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-10-18 22:13:37 +0100

    Remove sddm from ali-steam-deck

--- flake.nix
+++ flake.nix
@@ -220,7 +220,6 @@
           # ./app-profiles/desktop/display-managers/greetd
           # ./app-profiles/desktop/wms/sway
           ./app-profiles/desktop/aws
-          ./app-profiles/desktop/display-managers/sddm
           ./app-profiles/desktop/wms/plasma6
           ./app-profiles/desktop/wms/hyprland
           ./app-profiles/desktop/local-k8s
