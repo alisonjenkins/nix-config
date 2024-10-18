3758ee32c69465ae80af3a625bdc81d1a1b3792f
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-10-18 09:40:34 +0100

    Setup hyprland autostart

---
+++ home/wms/hyprland/configs/hypr/conf/autostart.conf.nix
@@ -0,0 +1,37 @@
+{pkgs}: ''
+  # -----------------------------------------------------
+  # Autostart
+  # -----------------------------------------------------
+
+  # Setup XDG for screen sharing
+  # exec-once = ~/.config/hypr/scripts/xdg.sh
+
+  # Start Polkit
+  # exec-once=/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+
+  # Load Dunst Notification Manager
+  exec-once = ${pkgs.dunst}/bin/dunst
+
+  # Restore wallpaper and launch waybar
+  # exec-once = ~/.config/hypr/scripts/wallpaper-restore.sh
+
+  # Load GTK settings
+  # exec-once = ~/.config/hypr/scripts/gtk.sh
+
+  # Using hypridle to start hyprlock
+  exec-once = ${pkgs.hypridle}/bin/hypridle
+
+  # Start eww daemon
+  exec-once = ${pkgs.eww}/bin/ags &
+
+  # Start autostart cleanup
+  # exec-once = ~/.config/hypr/scripts/cleanup.sh
+
+  # Start main tools
+  exec-once = ${pkgs._1password-gui}/bin/1password
+  exec-once = ${pkgs.alacritty}/bin/alacritty
+  exec-once = ${pkgs.firefox}/bin/firefox
+  exec-once = ${pkgs.steam}/bin/steam
+  exec-once = ${pkgs.vesktop}/bin/vesktop
+  exec-once = ${pkgs.vesktop}/bin/vesktop
+''
