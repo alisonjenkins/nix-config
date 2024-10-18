0040817bc2b708c521db7112dae22f1f8008a2a1
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-10-18 20:56:29 +0100

    Setup initial hyprland windowrules

---
+++ home/wms/hyprland/configs/hypr/conf/windowrules.conf.nix
@@ -0,0 +1,22 @@
+''
+  # -----------------------------------------------------
+  # Window rules
+  # -----------------------------------------------------
+
+  windowrule = float,^(pavucontrol)$
+  windowrule = float,^(blueman-manager)$
+  windowrule = float,^(nm-connection-editor)$
+  windowrule = float,^(nm-connection-editor)$
+
+  windowrulev2 = workspace 1, class:vesktop
+  windowrulev2 = workspace 2, class:Alacritty
+  windowrulev2 = fullscreenstate:* 2, class:Alacritty
+  windowrulev2 = workspace 3, class:firefox
+  windowrulev2 = workspace 4, class:steam
+  windowrulev2 = workspace 0, class:1Password
+
+  # Browser Picture in Picture
+  windowrulev2 = float, title:^(Picture-in-Picture)$
+  windowrulev2 = pin, title:^(Picture-in-Picture)$
+  windowrulev2 = move 69.5% 4%, title:^(Picture-in-Picture)$
+''
--- home/wms/hyprland/default.nix
+++ home/wms/hyprland/default.nix
@@ -10,6 +10,7 @@
     ".config/hypr/conf/hyprlock.conf".text = import ./configs/hypr/conf/hyprlock.conf.nix;
     ".config/hypr/conf/keybinds.conf".text = import ./configs/hypr/conf/keybinds.conf.nix {inherit pkgs;};
     ".config/hypr/conf/wlogout-layout.conf".text = import ./configs/hypr/conf/wlogout-layout.conf.nix {inherit pkgs;};
+    ".config/hypr/conf/windowrules.conf".text = import ./configs/hypr/conf/windowrules.conf.nix;
     ".config/hypr/hyprland.conf".text = builtins.readFile ./configs/hypr/hyprland.conf;
   };
 }
