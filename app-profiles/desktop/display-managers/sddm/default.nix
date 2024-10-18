6c3cf2da183c5c5c067fe8e11f8d8ee7963cb26b
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-10-18 09:14:04 +0100

    Add SDDM Avatar service

--- app-profiles/desktop/display-managers/sddm/default.nix
+++ app-profiles/desktop/display-managers/sddm/default.nix
@@ -14,4 +14,36 @@
       };
     };
   };
+
+  systemd.services = {
+    sddm-avatar = {
+      description = "Service to copy or update users Avatars at startup.";
+      wantedBy = ["multi-user.target"];
+      before = ["sddm.service"];
+      script = ''
+        set -eu
+        for user in /home/*; do
+            username=$(basename "$user")
+            user_icon_path="/home/$username/.face.icon"
+            accountservice_icon_path="/var/lib/AccountsService/icons/$username"
+            if [ -f "$user_icon_path" ]; then
+                if [ ! -f "$accountservice_icon_path" ]; then
+                    cp "$user_icon_path" "$accountservice_icon_path"
+                else
+                    if [ "$user_icon_path" -nt "$accountservice_icon_path" ]; then
+                        cp "$user_icon_path" "$accountservice_icon_path"
+                    fi
+                fi
+            fi
+        done
+      '';
+      serviceConfig = {
+        Type = "simple";
+        User = "root";
+        StandardOutput = "journal+console";
+        StandardError = "journal+console";
+      };
+    };
+    sddm = {after = ["sddm-avatar.service"];};
+  };
 }
