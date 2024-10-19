dcd7af6c125bc2bfff8cf9110a611c02c28d6f5b
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-10-19 11:13:11 +0100

    Swap to cachyos kernel on ali-steam-deck

--- hosts/ali-steam-deck/configuration.nix
+++ hosts/ali-steam-deck/configuration.nix
@@ -13,7 +13,7 @@
   ];
 
   boot = {
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_cachyos;
     loader = {
       efi.efiSysMountPoint = "/boot";
       grub = {
