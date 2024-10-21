6e0a3b39461c7ea37440402d33a196aabd33eaed
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-10-21 20:10:30 +0100

    Swap to cachyos lto kernel on ali-desktop

--- hosts/ali-desktop/configuration.nix
+++ hosts/ali-desktop/configuration.nix
@@ -23,8 +23,8 @@
     # kernelPackages = pkgs.linuxPackages-rt_latest;
     # kernelPackages = pkgs.linuxPackages;
     # kernelPackages = pkgs.linuxPackages_cachyos;
-    # kernelPackages = pkgs.linuxPackages_cachyos-lto;
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_cachyos-lto;
+    # kernelPackages = pkgs.linuxPackages_latest;
     # kernelPackages = pkgs.linuxPackages_xanmod;
     kernelParams = [
       "quiet"
