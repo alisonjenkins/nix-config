5e44c7bbc89f833b6b3108cbd8d334705d458458
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-10-22 17:53:24 +0100

    Add initial tlp settings for ali-laptop

--- hosts/ali-laptop/configuration.nix
+++ hosts/ali-laptop/configuration.nix
@@ -122,6 +122,26 @@
       pulse.enable = true;
     };
 
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 20;
+
+        #Optional helps save long term battery health
+        START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
+        STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+      };
+    };
+
     xserver = {
       videoDrivers = [
         "fbdev"
@@ -207,8 +227,9 @@
         extraGroups = ["networkmanager" "wheel" "docker"];
         packages = with pkgs; [
           firefox
-          neofetch
+          gwe
           lolcat
+          neofetch
         ];
       };
     };
