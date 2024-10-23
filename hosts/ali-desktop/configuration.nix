0207ccaeae14fa5c33a727f3fdb3290804a6f9e1
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-10-23 21:13:00 +0100

    Set nixPath on ali-desktop

--- hosts/ali-desktop/configuration.nix
+++ hosts/ali-desktop/configuration.nix
@@ -269,6 +269,7 @@
   nix = {
     package = pkgs.nixVersions.stable;
     extraOptions = "experimental-features = nix-command flakes";
+    nixPath = ["nixpkgs=${inputs.nixpkgs}"];
 
     gc = {
       automatic = true;
