3d08bc1314de78c7efa951ea75d232a8b8398c60
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-10-26 20:22:24 +0100

    Disable the gamescope wrapper to make it so Steam can launch games again

--- app-profiles/desktop/base/default.nix
+++ app-profiles/desktop/base/default.nix
@@ -157,14 +157,14 @@
     };
   };
 
-  security.wrappers = {
-    gamescope = {
-      owner = "root";
-      group = "root";
-      source = "${pkgs.gamescope}/bin/gamescope";
-      capabilities = "cap_sys_nice+ep";
-    };
-  };
+  # security.wrappers = {
+  #   gamescope = {
+  #     owner = "root";
+  #     group = "root";
+  #     source = "${pkgs.gamescope}/bin/gamescope";
+  #     capabilities = "cap_sys_nice+ep";
+  #   };
+  # };
 
   services = {
     atd.enable = true;
