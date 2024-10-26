74306527e220e6acd58c34a03e336ff2cedb0231
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-10-26 11:55:31 +0100

    Swap to sddm on ali-desktop

--- flake.nix
+++ flake.nix
@@ -132,9 +132,8 @@
             inherit system;
           };
           modules = [
-            # ./app-profiles/desktop/display-managers/sddm
             ./app-profiles/desktop/aws
-            ./app-profiles/desktop/display-managers/greetd
+            ./app-profiles/desktop/display-managers/sddm
             ./app-profiles/desktop/wms/plasma6
             ./app-profiles/desktop/wms/sway
             ./app-profiles/hardware/vr
