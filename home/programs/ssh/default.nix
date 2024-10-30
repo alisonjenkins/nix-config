cfcda2fd8c0e8d3c94929164e7997843e3d2b178
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-10-30 20:31:34 +0000

    Fix 1password ssh agent socket path under MacOS

--- home/programs/ssh/default.nix
+++ home/programs/ssh/default.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ pkgs, ... }: {
   home.file = {
     ".ssh/id_personal.pub".text = ''
       ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINqNVcWqkNPa04xMXls78lODJ21W43ZX6NlOtFENYUGF
@@ -7,15 +7,21 @@
     ".ssh/config".text =
       let
         use1password = true;
+        identity_sock_path =
+          if pkgs.stdenv.isLinux then
+            "~/.1password/agent.sock"
+          else if pkgs.stdenv.isDarwin then
+            "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\""
+          else "";
       in
       ''
         ${
-          (
+            (
             if use1password
-            then "Host *\n  IdentityAgent ~/.1password/agent.sock"
-            else ""
+            then "Host *\n  IdentityAgent ${identity_sock_path}"
+          else ""
           )
-        }
+          }
 
         Host home-kvm-hypervisor-1
           user ali
