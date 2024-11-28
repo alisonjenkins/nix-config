113084f81c54184a692fa4be165309a2dc323bc5
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-11-28 08:49:03 +0000

    Set ali-laptop to use my personal ssh key for git signing

--- flake.nix
+++ flake.nix
@@ -277,7 +277,7 @@
                 // {
                   gitUserName = "Alison Jenkins";
                   gitEmail = "1176328+alisonjenkins@users.noreply.github.com";
-                  gitGPGSigningKey = "";
+                  gitGPGSigningKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINqNVcWqkNPa04xMXls78lODJ21W43ZX6NlOtFENYUGF";
                 };
             }
           ];
