447673af7153ec38d553e7de6e073bdcfe7eca56
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-12-03 21:14:34 +0000

    Add hyprland nix substituters and trusted public key

--- hosts/ali-desktop/configuration.nix
+++ hosts/ali-desktop/configuration.nix
@@ -296,6 +296,8 @@
 
     settings = {
       auto-optimise-store = false;
+      substituters = [ "https://hyprland.cachix.org" ];
+      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
       trusted-users = [ "root" "@wheel" ];
     };
   };
