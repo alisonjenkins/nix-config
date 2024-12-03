b13434e8cde3c63ddfe85cf85bc3845dcd1a9ef8
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-12-03 21:02:42 +0000

    Sort hyprland dotfiles

--- home/wms/hyprland/default.nix
+++ home/wms/hyprland/default.nix
@@ -8,8 +8,8 @@
     ".config/hypr/conf/autostart.conf".text = import ./configs/hypr/conf/autostart.conf.nix { inherit pkgs; };
     ".config/hypr/conf/hyprlock.conf".text = import ./configs/hypr/conf/hyprlock.conf.nix;
     ".config/hypr/conf/keybinds.conf".text = import ./configs/hypr/conf/keybinds.conf.nix { inherit pkgs; };
-    ".config/hypr/conf/wleave-layout.conf".text = import ./configs/hypr/conf/wleave-layout.conf.nix { inherit pkgs; };
     ".config/hypr/conf/windowrules.conf".text = import ./configs/hypr/conf/windowrules.conf.nix;
+    ".config/hypr/conf/wleave-layout.conf".text = import ./configs/hypr/conf/wleave-layout.conf.nix { inherit pkgs; };
     ".config/hypr/hyprland.conf".text = builtins.readFile ./configs/hypr/hyprland.conf;
   };
 }
