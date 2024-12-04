0c85d47ea5b2452a8656fba5a7811d8fd0f1d70d
Alison Jenkins <alison.jenkins@brambles.com>
2024-12-04 10:30:40 +0000

    Ensure hyprland configs are not used on MacOS

--- home/wms/hyprland/default.nix
+++ home/wms/hyprland/default.nix
@@ -4,13 +4,14 @@
   # config.programs.hyprland.enable = true;
   wayland.windowManager.hyprland.systemd.enable = false;
 
-  home.file = {
-    ".config/hypr/conf/autostart.conf".text = import ./configs/hypr/conf/autostart.conf.nix { inherit pkgs; };
-    ".config/hypr/conf/hyprlock.conf".text = import ./configs/hypr/conf/hyprlock.conf.nix;
-    ".config/hypr/conf/keybinds.conf".text = import ./configs/hypr/conf/keybinds.conf.nix { inherit pkgs; };
-    ".config/hypr/conf/windowrules.conf".text = import ./configs/hypr/conf/windowrules.conf.nix;
-    ".config/hypr/conf/wleave-layout.conf".text = import ./configs/hypr/conf/wleave-layout.conf.nix { inherit pkgs; };
-    ".config/hypr/hyprland.conf".text = builtins.readFile ./configs/hypr/hyprland.conf;
-    ".config/xdg-desktop-portal/hyprland-portals.conf".text = builtins.readFile ./configs/xdg-desktop-portal/hyprland-portals.conf;
-  };
+  home.file =
+    if pkgs.stdenv.isLinux then {
+      ".config/hypr/conf/autostart.conf".text = import ./configs/hypr/conf/autostart.conf.nix { inherit pkgs; };
+      ".config/hypr/conf/hyprlock.conf".text = import ./configs/hypr/conf/hyprlock.conf.nix;
+      ".config/hypr/conf/keybinds.conf".text = import ./configs/hypr/conf/keybinds.conf.nix { inherit pkgs; };
+      ".config/hypr/conf/windowrules.conf".text = import ./configs/hypr/conf/windowrules.conf.nix;
+      ".config/hypr/conf/wleave-layout.conf".text = import ./configs/hypr/conf/wleave-layout.conf.nix { inherit pkgs; };
+      ".config/hypr/hyprland.conf".text = builtins.readFile ./configs/hypr/hyprland.conf;
+      ".config/xdg-desktop-portal/hyprland-portals.conf".text = builtins.readFile ./configs/xdg-desktop-portal/hyprland-portals.conf;
+    } else { };
 }
