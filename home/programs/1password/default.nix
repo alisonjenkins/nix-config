78301411c3c3b1b891280962eb2147a541ed6d22
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-12-07 14:07:51 +0000

    Rework 1password setup

--- home/programs/1password/default.nix
+++ home/programs/1password/default.nix
@@ -1,35 +1,4 @@
 { pkgs, ... }: {
-  home.packages =
-    if pkgs.stdenv.isLinux
-    then
-      with pkgs; [
-        _1password-cli
-        _1password-gui
-      ]
-    else [ ];
-
-  home.file =
-    if pkgs.stdenv.isLinux
-    then {
-      ".local/share/applications/1Password.desktop".text = ''
-        [Desktop Entry]
-        Comment[en_US]=
-        Comment=
-        Exec=${pkgs._1password-gui}/bin/1password
-        GenericName[en_US]=
-        GenericName=
-        Icon=${pkgs._1password-gui}/share/icons/hicolor/512x512/apps/1password.png
-        MimeType=
-        Name[en_US]=1Password
-        Name=1Password
-        Path=
-        StartupNotify=true
-        Terminal=false
-        TerminalOptions=
-        Type=Application
-        X-KDE-SubstituteUID=false
-        X-KDE-Username=
-      '';
-    }
-    else { };
+  programs._1password-gui.enable = if pkgs.stdenv.isLinux then true else false;
+  programs._1password-cli.enable = if pkgs.stdenv.isLinux then true else false;
 }
