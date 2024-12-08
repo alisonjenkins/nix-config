a5e73239438212eac232c6c8110079283a020ea9
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-12-08 23:45:16 +0000

    Change the home-k8s-master-1 sops config

--- hosts/home-k8s-master-1/configuration.nix
+++ hosts/home-k8s-master-1/configuration.nix
@@ -77,8 +77,8 @@
     defaultSopsFormat = "yaml";
 
     age = {
-      generateKey = true;
-      keyFile = "/var/lib/sops-nix/key.txt";
+      # generateKey = true;
+      # keyFile = "/var/lib/sops-nix/key.txt";
       sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
     };
 
