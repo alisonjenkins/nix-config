d596eda1f3bc8ceab1e19c46bfd816f8f7be98ad
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-12-08 22:39:48 +0000

    Patch home-k8s-master-1 kernel to support Netkit

--- hosts/home-k8s-master-1/hardware-configuration.nix
+++ hosts/home-k8s-master-1/hardware-configuration.nix
@@ -14,6 +14,15 @@
   boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPatches = [
+    {
+      name = "enable-netkit";
+      patch = null;
+      extraConfig = ''
+        NETKIT y
+      '';
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
