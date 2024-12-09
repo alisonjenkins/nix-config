09e2c50ddee46f44e851d74f1ea1f77e238e2909
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-12-09 08:38:11 +0000

    Remove tlp block fromm ali-framework-laptop
    Framework specifically say to use ppd instead.

--- hosts/ali-framework-laptop/configuration.nix
+++ hosts/ali-framework-laptop/configuration.nix
@@ -212,26 +212,6 @@
       enable = true;
     };
 
-    tlp = {
-      enable = false;
-      settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-
-        CPU_MIN_PERF_ON_AC = 0;
-        CPU_MAX_PERF_ON_AC = 100;
-        CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 20;
-
-        #Optional helps save long term battery health
-        START_CHARGE_THRESH_BAT0 = 40; # below this percentage it starts to charge
-        STOP_CHARGE_THRESH_BAT0 = 95; # above this percentage it stops charging
-      };
-    };
-
     xserver = {
       videoDrivers = [
         "fbdev"
