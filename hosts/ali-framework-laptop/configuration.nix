07fd12bc880491bf60defcd06eb42dfc62f7bc70
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-12-15 23:59:48 +0000

    Setup substituters for ali-framework-laptop

--- hosts/ali-framework-laptop/configuration.nix
+++ hosts/ali-framework-laptop/configuration.nix
@@ -150,6 +150,15 @@
     settings = {
       auto-optimise-store = false;
       trusted-users = [ "root" "@wheel" ];
+
+      substituters = [
+        "https://cosmic.cachix.org/"
+        "https://hyprland.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      ];
     };
   };
 
