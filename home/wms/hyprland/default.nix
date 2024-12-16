20864dd3f4da4933eb61176729c1f1d980cbbc0a
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-12-16 07:16:45 +0000

    Move hyprlock config

--- home/wms/hyprland/default.nix
+++ home/wms/hyprland/default.nix
@@ -15,7 +15,7 @@
     if pkgs.stdenv.isLinux then {
       ".config/hypr/autostart.conf".text = import ./configs/hypr/autostart.conf.nix { inherit pkgs; };
       ".config/hypr/hypridle.conf".text = import ./configs/hypr/hypridle.conf.nix;
-      ".config/hypr/hyprlock.conf".text = import ./configs/hypr/conf/hyprlock.conf.nix;
+      ".config/hypr/hyprlock.conf".text = import ./configs/hypr/hyprlock.conf.nix;
       ".config/hypr/conf/keybinds.conf".text = import ./configs/hypr/conf/keybinds.conf.nix { inherit pkgs; };
       ".config/hypr/conf/windowrules.conf".text = import ./configs/hypr/conf/windowrules.conf.nix;
       ".config/hypr/wleave-layout.conf".text = import ./configs/hypr/wleave-layout.conf.nix { inherit pkgs; };
