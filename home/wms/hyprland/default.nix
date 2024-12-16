da1881028af52a9f15658c9016d8d0c9377c8d50
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-12-16 07:18:11 +0000

    Move hyprland keybinds

--- home/wms/hyprland/default.nix
+++ home/wms/hyprland/default.nix
@@ -16,7 +16,7 @@
       ".config/hypr/autostart.conf".text = import ./configs/hypr/autostart.conf.nix { inherit pkgs; };
       ".config/hypr/hypridle.conf".text = import ./configs/hypr/hypridle.conf.nix;
       ".config/hypr/hyprlock.conf".text = import ./configs/hypr/hyprlock.conf.nix;
-      ".config/hypr/conf/keybinds.conf".text = import ./configs/hypr/conf/keybinds.conf.nix { inherit pkgs; };
+      ".config/hypr/keybinds.conf".text = import ./configs/hypr/keybinds.conf.nix { inherit pkgs; };
       ".config/hypr/conf/windowrules.conf".text = import ./configs/hypr/conf/windowrules.conf.nix;
       ".config/hypr/wleave-layout.conf".text = import ./configs/hypr/wleave-layout.conf.nix { inherit pkgs; };
       ".config/hypr/hyprland.conf".text = builtins.readFile ./configs/hypr/hyprland.conf;
