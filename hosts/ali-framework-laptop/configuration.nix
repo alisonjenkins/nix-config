c4fd50a30a94e2ef41a3615a607c2b40edb898ab
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-12-28 20:40:40 +0000

    Add more substituters to ali-framework-laptop

--- hosts/ali-framework-laptop/configuration.nix
+++ hosts/ali-framework-laptop/configuration.nix
@@ -154,10 +154,13 @@
       substituters = [
         "https://cosmic.cachix.org/"
         "https://hyprland.cachix.org"
+        "https://nix-community.cachix.org"
       ];
+
       trusted-public-keys = [
         "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
   };
