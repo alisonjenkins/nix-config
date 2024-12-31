88a49a0184f57c6580d98e0af32349fc830c0e32
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-12-31 23:27:53 +0000

    Delete grub theme definition for ali-work-laptop

--- hosts/ali-work-laptop/configuration.nix
+++ hosts/ali-work-laptop/configuration.nix
@@ -22,17 +22,6 @@
         efiInstallAsRemovable = true;
         efiSupport = true;
         useOSProber = true;
-        theme = pkgs.stdenv.mkDerivation {
-          pname = "distro-grub-themes";
-          version = "3.1";
-          src = pkgs.fetchFromGitHub {
-            owner = "AdisonCavani";
-            repo = "distro-grub-themes";
-            rev = "v3.1";
-            hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
-          };
-          installPhase = "cp -r customize/nixos $out";
-        };
       };
     };
   };
