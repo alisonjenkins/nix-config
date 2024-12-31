e0655c79c25b449ebaaba44214ea074f4e6dff1c
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-12-31 22:30:05 +0000

    Add nix aliases

--- home/shellAliases.nix
+++ home/shellAliases.nix
@@ -22,6 +22,10 @@
   "ll" = "${pkgs.eza}/bin/eza -l --grid --git";
   "ls" = "${pkgs.eza}/bin/eza";
   "lt" = "${pkgs.eza}/bin/eza --tree --git --long";
+  "nfu" = "nix flake update";
+  "nsg" = "nix store gc";
+  "nsgo" = "nix store gc && nix store optimise";
+  "nso" = "nix store optimise";
   "pwhash" = "python -c \"import crypt,getpass; print(crypt.crypt(getpass.getpass(), crypt.mksalt(crypt.METHOD_SHA512)))\"";
   "vi" = "nvim";
   "vim" = "nvim";
