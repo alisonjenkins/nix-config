5da7925ad9105662ca398134e43988380e8d1d68
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-01-05 15:50:19 +0000

    Add syctl options to ali-framework-laptop

--- hosts/ali-framework-laptop/configuration.nix
+++ hosts/ali-framework-laptop/configuration.nix
@@ -29,6 +29,59 @@
       "tc_cmos.use_acpi_alarm=1"
     ];
 
+    kernel.sysctl = {
+      # Network Perf Tuning
+      "net.core.netdev_max_backlog" = 16384;
+      # "net.core.netdev_budget" = 50000;
+      # "net.core.netdev_budget_usecs" = 5000;
+      "net.core.default_qdisc" = "cake";
+      "net.core.optmem_max" = 65536;
+      "net.core.rmem_default" = 1048576;
+      "net.core.rmem_max" = 16777216;
+      "net.core.somaxconn" = 8192;
+      "net.core.wmem_default" = 1048576;
+      "net.core.wmem_max" = 16777216;
+      "net.ipv4.conf.all.accept_redirects" = 0;
+      "net.ipv4.conf.all.log_martians" = 1;
+      "net.ipv4.conf.all.rp_filter" = 1;
+      "net.ipv4.conf.all.secure_redirects" = 0;
+      "net.ipv4.conf.all.send_redirects" = 0;
+      "net.ipv4.conf.default.accept_redirects" = 0;
+      "net.ipv4.conf.default.log_martians" = 1;
+      "net.ipv4.conf.default.rp_filter" = 1;
+      "net.ipv4.conf.default.secure_redirects" = 0;
+      "net.ipv4.conf.default.send_redirects" = 0;
+      "net.ipv4.icmp_echo_ignore_all" = 1;
+      "net.ipv4.tcp_congestion_control" = "bbr";
+      "net.ipv4.tcp_fastopen" = 3;
+      "net.ipv4.tcp_fin_timeout" = 10;
+      "net.ipv4.tcp_keepalive_intvl" = 10;
+      "net.ipv4.tcp_keepalive_probes" = 6;
+      "net.ipv4.tcp_keepalive_time" = 60;
+      "net.ipv4.tcp_max_syn_backlog" = 8192;
+      "net.ipv4.tcp_max_tw_buckets" = 2000000;
+      "net.ipv4.tcp_mtu_probing" = lib.mkForce 1;
+      "net.ipv4.tcp_rfc1337" = 1;
+      "net.ipv4.tcp_rmem" = "4096 1048576 2097152";
+      "net.ipv4.tcp_sack" = 1;
+      "net.ipv4.tcp_slow_start_after_idle" = 0;
+      "net.ipv4.tcp_syncookies" = 1;
+      "net.ipv4.tcp_timestamps" = 0;
+      "net.ipv4.tcp_tw_reuse" = 1;
+      "net.ipv4.tcp_wmem" = "4096 65536 16777216";
+      "net.ipv4.udp_rmem_min" = 8192;
+      "net.ipv4.udp_wmem_min" = 8192;
+      "net.ipv6.conf.all.accept_redirects" = 0;
+      "net.ipv6.conf.default.accept_redirects" = 0;
+      "net.net.ipv4.tcp_window_scaling" = 1;
+
+      # Virtual memory tuning
+      "vm.swappiness" = lib.mkForce 10;
+      "vm.dirty_ratio" = 10;
+      "vm.dirty_background_ratio" = 3;
+      "vm.vfs_cache_pressure" = 50;
+    };
+
     lanzaboote = {
       enable = true;
       pkiBundle = "/etc/secureboot";
