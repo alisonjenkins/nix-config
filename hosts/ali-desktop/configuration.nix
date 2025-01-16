6e4e116a5b9a8371f1f76af0fdbb45101d42ef8f
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-01-16 21:33:58 +0000

    Setup ssh hostkeys

--- hosts/ali-desktop/configuration.nix
+++ hosts/ali-desktop/configuration.nix
@@ -416,6 +416,18 @@
     openssh = {
       enable = true;
 
+      hostKeys = [
+        {
+          bits = 4096;
+          path = "/etc/ssh/ssh_host_rsa_key";
+          type = "rsa";
+        }
+        {
+          path = "/etc/ssh/ssh_host_ed25519_key";
+          type = "ed25519";
+        }
+      ];
+
       settings = {
         PasswordAuthentication = false;
         KbdInteractiveAuthentication = false;
