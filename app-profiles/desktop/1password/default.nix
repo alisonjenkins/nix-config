4fb9a0e9c3cb90704207032205fa4dc7bf8392e0
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-01-24 08:48:39 +0000

    Rework 1password module to allow all users on Linux to use it

--- app-profiles/desktop/1password/default.nix
+++ app-profiles/desktop/1password/default.nix
@@ -1,9 +1,13 @@
-{ pkgs, username, ... }: {
+{ pkgs, config, ... }: {
   programs._1password-gui =
-    if pkgs.stdenv.isLinux then {
-      enable = true;
-      polkitPolicyOwners = [ username ];
-    } else {
+    if pkgs.stdenv.isLinux then
+      let
+        op_polkit_owners = builtins.attrNames config.users;
+      in
+      {
+        enable = true;
+        polkitPolicyOwners = op_polkit_owners;
+      } else {
       enable = false;
     };
   programs._1password.enable = if pkgs.stdenv.isLinux then true else false;
