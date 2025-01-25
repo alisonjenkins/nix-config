c95581f6c78108048e7cbb388774bcc560d86886
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-01-25 14:12:17 +0000

    Setup nvidia driver on main profile of kal-laptop

--- hosts/kal-laptop/configuration.nix
+++ hosts/kal-laptop/configuration.nix
@@ -56,6 +56,29 @@
   hardware = {
     graphics.enable = true;
     pulseaudio.enable = false;
+
+    nvidia = {
+      modesetting.enable = true;
+      nvidiaSettings = true;
+      open = false;
+      package = config.boot.kernelPackages.nvidiaPackages.latest;
+      # package = config.boot.kernelPackages.nvidiaPackages.beta;
+
+      powerManagement = {
+        enable = true;
+        finegrained = true;
+      };
+
+      prime = {
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
+      };
+    };
   };
 
   networking = {
@@ -157,8 +180,9 @@
 
     xserver = {
       videoDrivers = [
-        "fbdev"
-        "modesetting"
+        # "fbdev"
+        # "modesetting"
+        "nvidia"
       ];
       xkb = {
         layout = "us";
