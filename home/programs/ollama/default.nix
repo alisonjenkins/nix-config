96dfbd724dd7280eb61dc1afcfad4dc53c1577ea
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-01-26 17:02:06 +0000

    Comment ollama home manager program module

--- home/programs/ollama/default.nix
+++ home/programs/ollama/default.nix
@@ -1,12 +1,12 @@
 { gpuType ? "", pkgs }: {
-  home.packages = (if gpuType == "amd" then
-    (with pkgs; [
-      ollama-rocm
-    ]) else if gpuType == "nvidia" then
-    (with pkgs; [
-      ollama-cuda
-    ]) else
-    (with pkgs; [
-      ollama
-    ]));
+  # home.packages = (if gpuType == "amd" then
+  #   (with pkgs; [
+  #     ollama-rocm
+  #   ]) else if gpuType == "nvidia" then
+  #   (with pkgs; [
+  #     ollama-cuda
+  #   ]) else
+  #   (with pkgs; [
+  #     ollama
+  #   ]));
 }
