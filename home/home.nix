cb2260af1ed236a2d7b7d37acad8840a5669c78e
Alison Jenkins <alison.jenkins@brambles.com>
2025-01-31 10:47:17 +0000

    Revert "Disable gpg agent config"
    This reverts commit 9d1ce27dfa58a45719737184864d51600cbdd080.

--- home/home.nix
+++ home/home.nix
@@ -112,19 +112,19 @@
 
   services = {
     ssh-agent.enable = lib.mkIf pkgs.stdenv.isLinux true;
-    # gpg-agent = {
-    #   enable = lib.mkIf pkgs.stdenv.isLinux true;
-    #   pinentryPackage = pkgs.kwalletcli.overrideAttrs (_: prev: {
-    #     meta =
-    #       prev.meta
-    #       // {
-    #         mainProgram = "pinentry-qt";
-    #       };
-    #   });
-    #   enableBashIntegration = true;
-    #   enableZshIntegration = true;
-    #   extraConfig = "pinentry-program ${pkgs.kwalletcli}/bin/pinentry-kwallet";
-    # };
+    gpg-agent = {
+      enable = lib.mkIf pkgs.stdenv.isLinux true;
+      pinentryPackage = pkgs.kwalletcli.overrideAttrs (_: prev: {
+        meta =
+          prev.meta
+          // {
+            mainProgram = "pinentry-qt";
+          };
+      });
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+      extraConfig = "pinentry-program ${pkgs.kwalletcli}/bin/pinentry-kwallet";
+    };
   };
 
   home.stateVersion = "24.05";
