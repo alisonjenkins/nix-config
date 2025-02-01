bc18e12972fa2d3bdd99da8c3e2fd52ab5bd8050
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-02-01 00:40:31 +0000

    Disable rocm override

--- modules/ollama/default.nix
+++ modules/ollama/default.nix
@@ -3,7 +3,7 @@
     ollama = {
       enable = true;
       acceleration = "rocm";
-      rocmOverrideGfx = "11.0.2";
+      # rocmOverrideGfx = "11.0.2";
       user = "ollama";
       group = "ollama";
     };
