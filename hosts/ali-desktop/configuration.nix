8993bffdc3e67860e869104563fabef807221a39
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-02-01 00:42:08 +0000

    Remove kal from ali-desktop

--- hosts/ali-desktop/configuration.nix
+++ hosts/ali-desktop/configuration.nix
@@ -341,12 +341,6 @@
       root = {
         hashedPasswordFile = "/persistence/passwords/root";
       };
-      kal = {
-        isNormalUser = true;
-        description = "Kal Zafar";
-        initialPassword = "initPw!";
-        extraGroups = [ "networkmanager" "wheel" "docker" ];
-      };
     };
   };
 
