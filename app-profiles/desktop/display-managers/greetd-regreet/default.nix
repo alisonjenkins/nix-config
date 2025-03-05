ef1196075d96e44e64ff1cf2672d4e08e3ccf0a2
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-03-05 23:01:22 +0000

    Fix greetd-regreet

--- app-profiles/desktop/display-managers/greetd-regreet/default.nix
+++ app-profiles/desktop/display-managers/greetd-regreet/default.nix
@@ -1,20 +1,31 @@
 { pkgs
+, lib
 , ...
 }: {
   services = {
     greetd = {
       enable = true;
-      settings = {
-        default_session = {
-          command = "${pkgs.greetd.regreet}/bin/regreet";
-        };
-      };
+      # settings = {
+      #   default_session = {
+      #     command = "${pkgs.cage}/bin/cage -s -- ${pkgs.greetd.regreet}/bin/regreet";
+      #   };
+      # };
+    };
+  };
+
+  programs = {
+    regreet = {
+      enable = true;
     };
   };
 
   security.pam.services.greetd.enableKwallet = true;
+  # system.activationScripts.makeRegreetLogDir = lib.stringAfter [ "var" ] ''
+  #   mkdir -p /var/log/regreet
+  #   chown greeter:greeter -R /var/log/regreet
+  # '';
 
-  environment.systemPackages = with pkgs; [
-    greetd.regreet
-  ];
+  # environment.systemPackages = with pkgs; [
+  #   greetd.regreet
+  # ];
 }
