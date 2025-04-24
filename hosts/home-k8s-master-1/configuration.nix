9fdad3550eb71853f11652592251493bd93f2192
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-04-24 17:05:35 +0100

    Disable cilium and use base k3s resources instead

--- hosts/home-k8s-master-1/configuration.nix
+++ hosts/home-k8s-master-1/configuration.nix
@@ -72,11 +72,11 @@
       extraFlags = toString [
         "--cluster-cidr=10.42.0.0/16"
         "--cluster-init"
-        "--disable servicelb"
-        "--disable traefik"
-        "--disable-kube-proxy"
-        "--disable-network-policy"
-        "--flannel-backend=none"
+        # "--disable servicelb"
+        # "--disable traefik"
+        # "--disable-kube-proxy"
+        # "--disable-network-policy"
+        # "--flannel-backend=none"
         "--service-cidr=10.43.0.0/16"
         "--write-kubeconfig-mode \"0400\""
       ];
@@ -84,9 +84,9 @@
 
       manifests =
         {
-          cilium = {
-            source = ../../k8s-setup/cilium-hr.yaml;
-          };
+          # cilium = {
+          #   source = ../../k8s-setup/cilium-hr.yaml;
+          # };
         };
     };
   };
