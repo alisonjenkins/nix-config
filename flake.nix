6773519de205d16e0ca57fdde36dd19ea37b52e7
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-06-09 14:47:34 +0100

    Flake input formatting

--- flake.nix
+++ flake.nix
@@ -52,34 +52,42 @@
       url = "github:nix-community/nix-on-droid/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     nur = {
       url = "github:nix-community/nur";
     };
+
     nixpkgs = {
       url = "github:nixos/nixpkgs/nixos-25.05";
       # url = "path:/home/ali/git/nixpkgs";
     };
+
     # nix-gaming = {
     #   url = "github:fufexan/nix-gaming";
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
+
     nix-index-database = {
       url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     # nix-rosetta-builder = {
     #   url = "github:cpick/nix-rosetta-builder";
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
+
     plasma-manager = {
       url = "github:pjones/plasma-manager";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
+
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     # umu = {
     #   url = "git+https://github.com/Open-Wine-Components/umu-launcher/?dir=packaging\/nix&submodules=1";
     #   inputs.nixpkgs.follows = "nixpkgs";
