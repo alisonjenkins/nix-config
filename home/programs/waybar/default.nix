cec5655750b7395a072c1360a6c620cf2db6737a
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-06-16 07:35:21 +0100

    Stop waybar being themed by stylix

--- home/programs/waybar/default.nix
+++ home/programs/waybar/default.nix
@@ -17,14 +17,14 @@
       then true
       else false;
     package = pkgs.waybar;
-    style =
-      if pkgs.stdenv.isLinux then
-        (with config.lib.stylix.colors.withHashtag; ''
-          @define-color base00 ${base00}; @define-color base01 ${base01}; @define-color base02 ${base02}; @define-color base03 ${base03};
-          @define-color base04 ${base04}; @define-color base05 ${base05}; @define-color base06 ${base06}; @define-color base07 ${base07};
-          @define-color base08 ${base08}; @define-color base09 ${base09}; @define-color base0A ${base0A}; @define-color base0B ${base0B};
-          @define-color base0C ${base0C}; @define-color base0D ${base0D}; @define-color base0E ${base0E}; @define-color base0F ${base0F};
-        '' + builtins.readFile ./style.css) else '''';
+    # style =
+    #   if pkgs.stdenv.isLinux then
+    #     (with config.lib.stylix.colors.withHashtag; ''
+    #       @define-color base00 ${base00}; @define-color base01 ${base01}; @define-color base02 ${base02}; @define-color base03 ${base03};
+    #       @define-color base04 ${base04}; @define-color base05 ${base05}; @define-color base06 ${base06}; @define-color base07 ${base07};
+    #       @define-color base08 ${base08}; @define-color base09 ${base09}; @define-color base0A ${base0A}; @define-color base0B ${base0B};
+    #       @define-color base0C ${base0C}; @define-color base0D ${base0D}; @define-color base0E ${base0E}; @define-color base0F ${base0F};
+    #     '' + builtins.readFile ./style.css) else '''';
 
     settings = [
       {
