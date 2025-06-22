76d4bfa845097edd1006cbf9ec0f36656921e3ec
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-06-22 22:47:24 +0100

    Add initial niri config

--- home/programs/niri/default.nix
+++ home/programs/niri/default.nix
@@ -1,4 +1,12 @@
-{ pkgs, ... }: {
+{ pkgs, inputs, system, ... }: {
+  home.packages = with pkgs; [
+    alacritty
+    fuzzel
+    swaylock
+    xwayland-satellite
+    inputs.quickshell.packages.${system}.default
+  ];
+
   home.file = {
     ".config/niri/config.kdl".source = ./config.kdl;
   };
