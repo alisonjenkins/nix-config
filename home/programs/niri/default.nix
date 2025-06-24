3af299d331cb8e978f56665ecb9ff7e5d75d49fa
Alison Jenkins <alison.jenkins@brambles.com>
2025-06-24 09:01:00 +0100

    Ensure that niri packages are not installed on MacOS

--- home/programs/niri/default.nix
+++ home/programs/niri/default.nix
@@ -1,10 +1,10 @@
 { pkgs, inputs, system, ... }: {
-  home.packages = with pkgs; [
-    fuzzel
-    swaylock
-    xwayland-satellite
+  home.packages =  if pkgs.stdenv.isLinux then [
+    pkgs.fuzzel
+    pkgs.swaylock
+    pkgs.xwayland-satellite
     inputs.quickshell.packages.${system}.default
-  ];
+  ] else [];
 
   home.file = {
     ".config/niri/config.kdl".source = ./config.kdl;
