565540d60ce6960d88b4db89e1096f778b59a5f0
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-06-29 12:12:32 +0100

    Setup initial keybinds for wlr-which-key on niri

--- home/programs/niri/default.nix
+++ home/programs/niri/default.nix
@@ -3,10 +3,12 @@
     inputs.quickshell.packages.${system}.default
     pkgs.fuzzel
     pkgs.swaylock
+    pkgs.unstable.wlr-which-key
     pkgs.xwayland-satellite
   ] else [];
 
   home.file = {
     ".config/niri/config.kdl".source = ./config.kdl;
+    ".config/wlr-which-key/config.yaml".source =./wlr-which-key/config.yaml;
   };
 }
