6c14ca63e7c8b910ae98536a245d251cdf15d7f6
Alison Jenkins <alison.jenkins@civica.com>
2025-07-18 16:49:27 +0100

    Setup newsboat AWS feeds

--- home/programs/newsboat/default.nix
+++ home/programs/newsboat/default.nix
@@ -5,12 +5,191 @@
     autoReload = true;
 
     urls = [
-      # {
-      #   url = "";
-      #   tags = [
-      #   ];
-      #   title = "";
-      # }
+      {
+        title = "Architecture";
+        url = "https://aws.amazon.com/blogs/architecture/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "AWS Cost Management";
+        url = "https://aws.amazon.com/blogs/aws-cost-management/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "AWS Partner Network";
+        url = "https://aws.amazon.com/blogs/apn/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "AWS Podcast";
+        url = "https://aws.amazon.com/podcasts/aws-podcast/";
+        tags = ["aws"];
+      }
+      {
+        title = "AWS Marketplace";
+        url = "https://aws.amazon.com/blogs/awsmarketplace/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "AWS News";
+        url = "https://aws.amazon.com/blogs/aws/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Big Data";
+        url = "https://aws.amazon.com/blogs/big-data/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Business Productivity";
+        url = "https://aws.amazon.com/blogs/business-productivity/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Compute";
+        url = "https://aws.amazon.com/blogs/compute/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Contact Center";
+        url = "https://aws.amazon.com/blogs/contact-center/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Containers";
+        url = "https://aws.amazon.com/blogs/containers/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Database";
+        url = "https://aws.amazon.com/blogs/database/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Desktop & Application Streaming";
+        url = "https://aws.amazon.com/blogs/desktop-and-application-streaming/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Developer";
+        url = "https://aws.amazon.com/blogs/developer/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "DevOps";
+        url = "https://aws.amazon.com/blogs/devops/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Enterprise Strategy";
+        url = "https://aws.amazon.com/blogs/enterprise-strategy/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Front-End Web & Mobile";
+        url = "https://aws.amazon.com/blogs/mobile/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Game Tech";
+        url = "https://aws.amazon.com/blogs/gametech/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "HPC";
+        url = "https://aws.amazon.com/blogs/hpc/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Infrastructure & Automation";
+        url = "https://aws.amazon.com/blogs/infrastructure-and-automation/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Industries";
+        url = "https://aws.amazon.com/blogs/industries/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Internet of Things";
+        url = "https://aws.amazon.com/blogs/iot/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Machine Learning";
+        url = "https://aws.amazon.com/blogs/machine-learning/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Management & Governance";
+        url = "https://aws.amazon.com/blogs/mt/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Media";
+        url = "https://aws.amazon.com/blogs/media/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Messaging & Targeting";
+        url = "https://aws.amazon.com/blogs/messaging-and-targeting/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Networking & Content Delivery";
+        url = "https://aws.amazon.com/blogs/networking-and-content-delivery/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Open Source";
+        url = "https://aws.amazon.com/blogs/opensource/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Public Sector";
+        url = "https://aws.amazon.com/blogs/publicsector/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Quantum Computing";
+        url = "https://aws.amazon.com/blogs/quantum-computing/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Robotics";
+        url = "https://aws.amazon.com/blogs/robotics/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "SAP";
+        url = "https://aws.amazon.com/blogs/awsforsap/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Security, Identity, & Compliance";
+        url = "https://aws.amazon.com/blogs/security/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Startups";
+        url = "https://aws.amazon.com/blogs/startups/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Storage";
+        url = "https://aws.amazon.com/blogs/storage/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Training & Certification";
+        url = "https://aws.amazon.com/blogs/training-and-certification/feed/";
+        tags = ["aws"];
+      }
+      {
+        title = "Windows on AWS";
+        url = "https://aws.amazon.com/blogs/modernizing-with-aws/feed/";
+        tags = ["aws"];
+      }
     ];
   };
 }
