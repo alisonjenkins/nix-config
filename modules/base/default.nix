b7f7cfcb36883a55972ff6a71d8997f18083a1ac
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-08-29 08:44:23 +0100

    Include Cachix config for ajenkins-public as part of base

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -14,7 +14,7 @@
   imports = [
     inputs.chaotic.nixosModules.default
     inputs.impermanence.nixosModules.impermanence
-  ];
+  ] ++ lib.optional (builtins.fileExists /etc/nixos/cachix/ajenkins-public.nix) [ /etc/nixos/cachix/ajenkins-public.nix ];
   console.keyMap = consoleKeyMap;
 
   time.timeZone = timezone;
