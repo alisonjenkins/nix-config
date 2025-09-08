f948ee43c7d0a0f7e1f1c8487e0015cdfb036fcb
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-09-08 22:30:24 +0100

    Remove duplicate keybase autostart

--- home/programs/keybase/default.nix
+++ home/programs/keybase/default.nix
@@ -6,27 +6,27 @@
     keybase-gui
   ] else [];
 
-  home.file =
-    if pkgs.stdenv.isLinux then {
-      ".local/share/applications/Keybase.desktop".text = ''
-        [Desktop Entry]
-        Comment[en_US]=
-        Comment=
-        Exec=${pkgs.keybase-gui}/bin/keybase-gui
-        GenericName[en_US]=
-        GenericName=
-        Icon=${pkgs.keybase-gui}/share/icons/hicolor/128x128/apps/keybase.png
-        MimeType=
-        Name[en_US]=Keybase
-        Name=Keybase
-        Path=
-        StartupNotify=true
-        Terminal=false
-        TerminalOptions=
-        Type=Application
-        X-KDE-SubstituteUID=false
-        X-KDE-Username=
-      '';
-    } else { };
+  # home.file =
+  #   if pkgs.stdenv.isLinux then {
+  #     ".local/share/applications/Keybase.desktop".text = ''
+  #       [Desktop Entry]
+  #       Comment[en_US]=
+  #       Comment=
+  #       Exec=${pkgs.keybase-gui}/bin/keybase-gui
+  #       GenericName[en_US]=
+  #       GenericName=
+  #       Icon=${pkgs.keybase-gui}/share/icons/hicolor/128x128/apps/keybase.png
+  #       MimeType=
+  #       Name[en_US]=Keybase
+  #       Name=Keybase
+  #       Path=
+  #       StartupNotify=true
+  #       Terminal=false
+  #       TerminalOptions=
+  #       Type=Application
+  #       X-KDE-SubstituteUID=false
+  #       X-KDE-Username=
+  #     '';
+  #   } else { };
 }
 
