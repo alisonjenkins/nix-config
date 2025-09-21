012ecd229b0809aa893dead40f9bb6b9059d2109
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-09-21 22:11:19 +0100

    refactor: comment localstack
    Too many things locked behind pay walls.

--- home/programs/aws/default.nix
+++ home/programs/aws/default.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
+    # localstack
     aws-sam-cli
     awscli2
     ec2-metadata-mock
     git-remote-codecommit
-    localstack
     packer
   ];
 }
