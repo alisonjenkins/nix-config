d8899e8fd47197208a65753d401feb9d55104a51
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-09-28 22:59:43 +0100

    fix: pkgs overlay

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,7 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
-pkgs: {
+{pkgs, ...}: {
   # example = pkgs.callPackage ./example { };
   git-clean = pkgs.callPackage ./git-clean { inherit pkgs; };
-  wallpapers = pkgs.callPackage ./wallpapers { };
+  wallpapers = pkgs.callPackage ./wallpapers {} ;
 }
