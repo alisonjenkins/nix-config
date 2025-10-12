c5d3fca54b102f8e754cedbd9732f339b181adb8
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-10-12 17:53:30 +0100

    feat: allow setting grace on lock-session

--- pkgs/lock-session/default.nix
+++ pkgs/lock-session/default.nix
@@ -5,14 +5,23 @@
   ...
 }:
 writeShellScriptBin "lock-session" ''
-#!/bin/sh
-${procps}/bin/pidof swaylock || ${swaylock-effects}/bin/swaylock \
-  --clock \
-  --daemonize \
-  --effect-blur 5x4 \
-  --grace 5 \
-  --grace-no-mouse \
-  --grace-no-touch \
-  --indicator-idle-visible \
-  --screenshots
+  #!/bin/bash
+  GRACE_SECONDS="''${1:-}"
+
+  SWAYLOCK_ARGS=(
+    "--daemonize"
+    "--effect-blur"
+    "5x4"
+    "--indicator-idle-visible"
+    "--screenshots"
+  )
+
+  if [[ "$GRACE_SECONDS" ]]; then
+    SWAYLOCK_ARGS+=("--grace")
+    SWAYLOCK_ARGS+=("$GRACE_SECONDS")
+    SWAYLOCK_ARGS+=("--grace-no-mouse")
+    SWAYLOCK_ARGS+=("--grace-no-touch")
+  fi
+
+  ${procps}/bin/pidof swaylock || ${swaylock-effects}/bin/swaylock ''${SWAYLOCK_ARGS[@]}
 ''
