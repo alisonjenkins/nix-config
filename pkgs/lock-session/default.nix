c08efc0072dda4b09117a8c8859c062c2d5da11b
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-10-12 10:46:22 +0100

    feat: add daemonize option to swaylock lock-session command
    This helps ensure that the lock-session command can run in the
    background for the sake of swayidle.

--- pkgs/lock-session/default.nix
+++ pkgs/lock-session/default.nix
@@ -8,6 +8,7 @@ writeShellScriptBin "lock-session" ''
 #!/bin/sh
 ${procps}/bin/pidof swaylock || ${swaylock-effects}/bin/swaylock \
   --clock \
+  --daemonize \
   --effect-blur 5x4 \
   --grace 5 \
   --grace-no-mouse \
