c806629ed7aa6741b8b3c4a7afc666b307c5ac97
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-11-30 18:06:17 +0000

    fix: remove deprecated ovmf option

--- modules/libvirtd/default.nix
+++ modules/libvirtd/default.nix
@@ -8,16 +8,16 @@
       package = pkgs.qemu_kvm;
       runAsRoot = true;
       swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [
-          (pkgs.OVMF.override {
-            secureBoot = true;
-            tpmSupport = true;
-          }).fd
-        ];
-      };
       vhostUserPackages = [ pkgs.virtiofsd ];
     };
+  };
+
+  environment = {
+    systemPackages = [
+      (pkgs.OVMF.override {
+        secureBoot = true;
+        tpmSupport = true;
+      })
+    ];
   };
 }
