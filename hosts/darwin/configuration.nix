e7e9fe4d9dc9e23479c5134d41be2ec410db0d16
Alison Jenkins <alison.jenkins@civica.com>
2025-12-07 10:57:50 +0000

    fix: setup trusted substituters for darwin

--- hosts/darwin/configuration.nix
+++ hosts/darwin/configuration.nix
@@ -580,6 +580,7 @@
         "https://nix-community.cachix.org"
         "https://nixpkgs-cross-overlay.cachix.org"
       ];
+      trusted-substituters = [ "https://hydra.nixos.org/" ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
