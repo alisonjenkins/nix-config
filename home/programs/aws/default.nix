0a9ebb66a61314553245ac0c730a2dc1ee64582c
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-12-08 07:58:41 +0000

    fix: disable aws sam cli as it is broken on 25.11

--- home/programs/aws/default.nix
+++ home/programs/aws/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     # localstack
-    aws-sam-cli
+    # aws-sam-cli  # Temporarily disabled due to dependency conflicts in 25.11
     awscli2
     ec2-metadata-mock
     git-remote-codecommit
