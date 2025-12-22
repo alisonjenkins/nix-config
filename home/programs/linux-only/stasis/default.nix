75353ee4f3dab8e921ad2f5c7af652eb4d3103e0
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-12-22 09:32:43 +0000

    fix: setup stasis

---
+++ home/programs/linux-only/stasis/default.nix
@@ -0,0 +1,33 @@
+{
+  bluetoothHeadsetMac ? "",
+  pkgs,
+  ...
+}: {
+  home.packages = [
+    pkgs.stasis
+  ];
+
+  home.file = {
+    ".config/stasis/stasis.rune".text = (import ./stasis.rune.nix { inherit bluetoothHeadsetMac pkgs; });
+  };
+
+  systemd.user.services.stasis = {
+    Unit = {
+      Description = "Stasis Wayland Idle Manager";
+      After = [ "graphical-session.target" ];
+      Wants = [ "graphical-session.target" ];
+    };
+
+    Service = {
+      Type = "simple";
+      ExecStart = "${pkgs.stasis}/bin/stasis";
+      Restart = "always";
+      RestartSec = 5;
+      Environment = "WAYLAND_DISPLAY=wayland-0";
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
+}
