44847c961f3001ebf8430481ea597f5ca890c6a9
Test User <test@test.com>
2025-12-22 11:10:50 +0000

    fix: reformat stasis config

--- home/programs/linux-only/stasis/stasis.rune.nix
+++ home/programs/linux-only/stasis/stasis.rune.nix
@@ -13,6 +13,7 @@
     pre_suspend_command "${pkgs.suspendScripts}/bin/suspend-pre"
     respect_idle_inhibitors true
     resume_command "${pkgs.suspendScripts}/bin/suspend-resume '${bluetoothHeadsetMac}'"
+
     inhibit_apps [
       "mpv"
       r".*[Vv]ideo.*"
@@ -21,16 +22,18 @@
       r"firefox.*"
       r"steam_app_.*"
     ]
+
+    dpms:
+      timeout = 330
+      command "niri msg action power-off-monitors"
+      resume_command "niri msg action power-on-monitors"
+    end
+
     lock_screen:
       command "${pkgs.lock-session}/bin/lock-session ${toString lockGracePeriodSeconds} ${toString lockFadeInSeconds}"
       lock_command "${pkgs.lock-session}/bin/lock-session ${toString idleLockGracePeriodSeconds} ${toString lockFadeInSeconds}"
       resume_command "notify-send 'Welcome back!'"
       timeout = 900
-    end
-    dpms:
-      timeout = 330
-      command "niri msg action power-off-monitors"
-      resume_command "niri msg action power-on-monitors"
     end
   end
 ''
