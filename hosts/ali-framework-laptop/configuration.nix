7745bab46b87f61a892e7ca17b757fc7abdcfad2
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-12-28 08:18:50 +0000

    feat: swap ali-framework-laptop to liquorix kernel

--- hosts/ali-framework-laptop/configuration.nix
+++ hosts/ali-framework-laptop/configuration.nix
@@ -37,12 +37,11 @@
     bootspec.enableValidation = true;
     # kernelPackages = pkgs.linuxPackages-rt_latest;
     # kernelPackages = pkgs.linuxPackages;
-    # kernelPackages = pkgs.linuxPackages_cachyos-lto;
-    # kernelPackages = pkgs.linuxPackages_cachyos;
+    # kernelPackages = pkgs.linuxPackages_latest;
     # kernelPackages = pkgs.linuxPackages_testing;
     # kernelPackages = pkgs.linuxPackages_xanmod;
     # kernelPackages = pkgs.linuxPackages_zen;
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_lqx;
 
     kernelParams = [
       # "mem_sleep_default=deep"
