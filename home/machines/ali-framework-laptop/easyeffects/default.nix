20a61374470df4a175c843286e742334f44c22d8
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2026-01-07 13:42:22 +0000

    fix: quiet volume on ali-framework-laptop

--- home/machines/ali-framework-laptop/easyeffects/default.nix
+++ home/machines/ali-framework-laptop/easyeffects/default.nix
@@ -20,6 +20,18 @@
       ".config/easyeffects/output/${filename}"
       {source = profilesDir + "/${filename}";}
   ) profileFiles;
+
+  # Framework DSP profile from https://github.com/cab404/framework-dsp
+  frameworkDspProfile = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/cab404/framework-dsp/refs/heads/master/config/output/Gracefu's%20Edits.json";
+    sha256 = "sha256-KZ8L1oEaeffLq9JkQ4lajwKXE34w8PdmmCs+NX44xkY=";
+    name = "gracefus-edits.json";
+  };
+
+  frameworkDspImpulseResponse = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/cab404/framework-dsp/refs/heads/master/config/irs/IR_22ms_27dB_5t_15s_0c.irs";
+    sha256 = "sha256-IxDXNhnTg/NrhPxA5+6u/meEnlX720eoQPyoJfbuge0=";
+  };
 in {
   home.packages = [pkgs.easyeffects];
 
@@ -27,5 +39,11 @@ in {
     profileFileAttrs
     // {
       ".config/easyeffects/output/.keep".text = "";
+      ".local/share/easyeffects/output/Gracefu's Edits.json" = {
+        text = builtins.readFile frameworkDspProfile;
+      };
+      ".local/share/easyeffects/irs/IR_22ms_27dB_5t_15s_0c.irs" = {
+        source = frameworkDspImpulseResponse;
+      };
     };
 }
