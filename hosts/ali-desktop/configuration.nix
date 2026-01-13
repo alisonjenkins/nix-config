79ac6909778656a080dce2dd3fcf4a68a4f35c04
Test User <test@test.com>
2026-01-13 23:18:47 +0000

    fix: increase loadavg-target for beesd on ali-desktop

--- hosts/ali-desktop/configuration.nix
+++ hosts/ali-desktop/configuration.nix
@@ -16,7 +16,7 @@
           spec = "LABEL=persistence";
           hashTableSizeMB = 2048;
           verbosity = "crit";
-          extraOptions = [ "--loadavg-target" "5.0" ];
+          extraOptions = [ "--loadavg-target" "15.0" ];
         };
       };
       inherit inputs lib outputs pkgs;
