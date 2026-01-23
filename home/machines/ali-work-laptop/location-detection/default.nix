8316a956fb7f8e9d86e0adf2a53821fd5d59b5a1
Test User <test@test.com>
2026-01-23 01:16:35 +0000

    fix: location-detection secrets detection

--- home/machines/ali-work-laptop/location-detection/default.nix
+++ home/machines/ali-work-laptop/location-detection/default.nix
@@ -6,17 +6,26 @@
   ...
 }: let
   secretsFile = ../../../../secrets/${hostname}/locations.yaml;
+  secretsFileExists = builtins.pathExists secretsFile;
 in {
   home.packages = [pkgs.detect-location];
 
   sops.age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
 
-  sops.secrets."locations-config" = {
-    path = "${config.home.homeDirectory}/.config/location-detection/locations.yaml";
-    sopsFile = secretsFile;
-    format = "yaml";
-    key = "";
+  sops.secrets = lib.mkIf secretsFileExists {
+    "locations-config" = {
+      path = "${config.home.homeDirectory}/.config/location-detection/locations.yaml";
+      sopsFile = secretsFile;
+      format = "yaml";
+      key = "";
+    };
   };
 
-  home.sessionVariables.LOCATION_CONFIG = "${config.home.homeDirectory}/.config/location-detection/locations.yaml";
+  home.sessionVariables = lib.mkIf secretsFileExists {
+    LOCATION_CONFIG = "${config.home.homeDirectory}/.config/location-detection/locations.yaml";
+  };
+
+  warnings = lib.mkIf (!secretsFileExists) [
+    "Location detection: secrets/${hostname}/locations.yaml not found. Create it using the template in home/machines/locations.yaml.template"
+  ];
 }
