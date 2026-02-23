b02d68f5334242ffd41b47f46a2ffd75440643e9
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2026-02-23 13:09:00 +0000

    fix: set follows on various inputs

--- flake.nix
+++ flake.nix
@@ -4,19 +4,43 @@
   inputs = {
     # ali-neovim.url = "git+file:///home/ali/git/neovim-nix-flake";
     # niri.url = "github:YaLTeR/niri";
-    eks-creds.url = "github:alisonjenkins/eks-creds";
+    eks-creds = {
+      url = "github:alisonjenkins/eks-creds";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-parts.url = "github:hercules-ci/flake-parts";
     haumea = {
       url = "github:nix-community/haumea/v0.2.2";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    impermanence.url = "github:nix-community/impermanence";
-    jovian-nixos.url = "github:Jovian-Experiments/Jovian-NixOS";
-    niri-flake.url = "github:sodiboo/niri-flake/27e012b4cd49e9ac438573ec7a6db3e5835828c3";
-    nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel/release";
+    impermanence = {
+      url = "github:nix-community/impermanence";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+      };
+    };
+    jovian-nixos = {
+      url = "github:Jovian-Experiments/Jovian-NixOS";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    niri-flake = {
+      url = "github:sodiboo/niri-flake/27e012b4cd49e9ac438573ec7a6db3e5835828c3";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        nixpkgs-stable.follows = "nixpkgs_stable";
+      };
+    };
+    nix-cachyos-kernel = {
+      url = "github:xddxdd/nix-cachyos-kernel/release";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nix-colors.url = "github:misterio77/nix-colors";
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
-    nixgl.url = "github:nix-community/nixGL";
+    nixgl = {
+      url = "github:nix-community/nixGL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     nixpkgs_lqx_pin.url = "github:nixos/nixpkgs/82000a14b7ec4009a25cbf8d3d49bcb4a6a85e41";
     nixpkgs_master.url = "github:nixos/nixpkgs";
@@ -24,16 +48,22 @@
     nixpkgs_old.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs_stable_darwin.url = "github:nixos/nixpkgs/nixpkgs-25.11-darwin";
     nixpkgs_unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixvirt.url = "github:AshleyYakeley/NixVirt/v0.6.0";
-    stylix.url = "github:danth/stylix/release-25.11";
+    nixvirt = {
+      url = "github:AshleyYakeley/NixVirt/v0.6.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    stylix = {
+      url = "github:danth/stylix/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     ali-neovim = {
       url = "github:alisonjenkins/neovim-nix-flake";
-      # inputs = {
-      #   nixpkgs.follows = "nixpkgs_unstable";
-      #   nixpkgs-master.follows = "nixpkgs_master";
-      #   nixpkgs-stable.follows = "nixpkgs_stable";
-      # };
+      inputs = {
+        nixpkgs.follows = "nixpkgs_unstable";
+        nixpkgs-master.follows = "nixpkgs_master";
+        nixpkgs-stable.follows = "nixpkgs_stable";
+      };
     };
 
     darwin = {
@@ -76,6 +106,7 @@
 
     nixcord = {
       url = "github:kaylorben/nixcord/a8802dc23e112f98196a7daa68f0e246c7a0ea64";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nix-on-droid = {
@@ -92,6 +123,7 @@
 
     nur = {
       url = "github:nix-community/nur";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nixpkgs = {
@@ -140,9 +172,10 @@
 
     tmux-sessionizer = {
       url = "github:alisonjenkins/tmux-sessionizer/b9965259166c588479c01328e34f0fbc98fa03a2";
-      # inputs = {
-      #   nixpkgs.follows = "nixpkgs_unstable";
-      # };
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        rust-overlay.follows = "rust-overlay";
+      };
     };
 
     # umu = {
