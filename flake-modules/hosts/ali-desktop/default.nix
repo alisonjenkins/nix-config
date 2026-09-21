761e70d4bcad4703c1aa68bdc25c6fb21aad50c1
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2026-09-21 16:40:54 +0100

    feat(ali-desktop): enable declarative beatsaber mod install

--- flake-modules/hosts/ali-desktop/default.nix
+++ flake-modules/hosts/ali-desktop/default.nix
@@ -97,7 +97,7 @@ in {
         home-manager.useGlobalPkgs = true;
         home-manager.useUserPackages = true;
         home-manager.users.${specialArgs.username} = {
-          imports = [ self.homeModules.home-linux self.homeModules.vr self.homeModules.subnautica-vr ];
+          imports = [ self.homeModules.home-linux self.homeModules.vr self.homeModules.subnautica-vr self.homeModules.beatsaber ];
 
           home.packages = [
             pkgs.lmstudio
